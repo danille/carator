dc42c1ef796c5d8563d61690fa6e129af9cede3d
Danylo Lebediev <danilleock@gmail.com>
2018-08-15 12:51:41 +0200

    Added Homepage + Devise setup

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,54 @@
+Given("I visit the homepage") do
+  visit root_path
+end
+
+When("I fill in the sign up form") do
+  click_link "Sign up"
+
+  fill_in "user_email", with: "tester@testdomain.com"
+  fill_in "user_password", with: "pa$$w0rd"
+  fill_in "user_password_confirmation", with: "pa$$w0rd"
+  click_button “Sign up”
+end
+
+When("I confirm the email") do
+  open_email "tester@testdomain.com"
+
+  visit_in_email "Confirm my account"
+end
+
+Then("should see that my account is confirmed") do
+  message = "Your email address has been successfully confirmed"
+
+  expect(page).to have_content(message)
+end
+
+Given("I am a registered user") do
+  @registered_user = FactoryBot.create(:user, name: "tester@testdomain.com", password: "pa$$w0rd")
+end
+
+When("I fill in a log in form") do
+  fill_in "user_email", with: "tester@testdomain.com"
+  fill_in "user_password", with: "pa$$w0rd"
+  click_button "Log in"
+end
+
+Then("I should be logged in") do
+  expect(page).to have_content("Logged in")
+end
+
+Given("I am a logged in") do
+  visit root_path
+
+  fill_in "user_email", with: "tester@testdomain.com"
+  fill_in "user_password", with: "pa$$w0rd"
+  click_button "Log in"
+end
+
+When("I click log out button") do
+  click_button "Log out"
+end
+
+Then("I should be redirected to the log in page") do
+  expect(page).to have_content("Log in")
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'support/controller_helpers'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -31,6 +32,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.include Warden::Test::Helpers
+  config.include Devise::TestHelpers, :type => :controller
+  config.include ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
