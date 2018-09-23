f309b7a352d12bb49c0ce7d5c48c010da870b5db
Danylo Lebediev <danilleock@gmail.com>
2018-09-23 18:34:57 +0200

    Titleizing manufacturer name

--- app/models/car.rb
+++ app/models/car.rb
@@ -2,10 +2,8 @@ class Car < ApplicationRecord
   before_save :capitalize_manufacturer_name
   belongs_to :user
 
-  private
-
   def capitalize_manufacturer_name
-    manufacturer.capitalize!
+    manufacturer.titleize!
   end
 
 end
