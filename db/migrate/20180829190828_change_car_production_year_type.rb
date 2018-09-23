38d60f805419b7dbd1c7e0122bcdd1a74cbd1346
Danylo Lebediev <danilleock@gmail.com>
2018-09-23 17:25:16 +0200

    Add data type to column definition

--- db/migrate/20180829190828_change_car_production_year_type.rb
+++ db/migrate/20180829190828_change_car_production_year_type.rb
@@ -1,6 +1,6 @@
 class ChangeCarProductionYearType < ActiveRecord::Migration[5.1]
   def change
     remove_column :cars, :production_year
-    add_column :cars, :production_year
+    add_column :cars, :production_year, :integer
   end
 end
