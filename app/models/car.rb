ea93391a192af25d785b9b0a96e48be89686420f
Danylo Lebediev <danilleock@gmail.com>
2018-09-23 17:59:38 +0200

    Toggle test for Cars show method + little code improvements

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,11 @@
 class Car < ApplicationRecord
+  before_save :capitalize_manufacturer_name
   belongs_to :user
+
+  private
+
+  def capitalize_manufacturer_name
+    manufacturer.capitalize!
+  end
+
 end
